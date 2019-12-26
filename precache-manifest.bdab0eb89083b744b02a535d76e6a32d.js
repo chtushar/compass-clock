a03a716db6553f6df888219812022180946268de
Tushar Choudhari <choudharit23@gmail.com>
2019-12-26 22:52:29 +0530

    Updates

---
+++ precache-manifest.bdab0eb89083b744b02a535d76e6a32d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb3805f61bd6c1498c139346f48c247",
+    "url": "/compassclock/index.html"
+  },
+  {
+    "revision": "3604c4925644bcaad7f5",
+    "url": "/compassclock/static/css/main.18af3af1.chunk.css"
+  },
+  {
+    "revision": "69bc0a0413e7062c88db",
+    "url": "/compassclock/static/js/2.71a0948a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/compassclock/static/js/2.71a0948a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3604c4925644bcaad7f5",
+    "url": "/compassclock/static/js/main.c2546bb8.chunk.js"
+  },
+  {
+    "revision": "742f9a9d0c8a76f702bf",
+    "url": "/compassclock/static/js/runtime-main.6d2dde70.js"
+  }
+]);
